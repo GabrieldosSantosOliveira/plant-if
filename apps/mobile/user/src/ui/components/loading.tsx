bd4657896cc6607d2e514221b58eab82ecf59817
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-07-28 22:14:07 -0300

    chore(server): set engine node  version 18

--- apps/mobile/user/src/ui/components/loading.tsx
+++ apps/mobile/user/src/ui/components/loading.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { ActivityIndicator } from 'react-native'
 
+import { useTheme } from '../hooks/use-theme'
 import { Box } from './shared/box'
 
 export const Loading = () => {
+  const { colors } = useTheme()
   return (
-    <Box flex={1} justifyContent="center" alignItems="center">
-      <ActivityIndicator size="large" color={'black'} />
+    <Box
+      flex={1}
+      backgroundColor="main-background"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <ActivityIndicator size="large" color={colors['icon-main']} />
     </Box>
   )
 }
