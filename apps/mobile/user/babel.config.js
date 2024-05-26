380ea86f86af8bdf0f4a761879111f933876c541
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2024-05-26 13:33:12 -0400

    chore(mobile): remove package babel-plugin-module-resolver to handle path alias
    Expo since version 50 handle path alias by default

--- apps/mobile/user/babel.config.js
+++ apps/mobile/user/babel.config.js
@@ -2,31 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@/@types/*': './src/@types',
-            '@/constants': './src/constants',
-            '@/data': './src/data',
-            '@/domain': './src/domain',
-            '@/errors': './src/errors',
-            '@/helpers': './src/helpers',
-            '@/infra': './src/infra',
-            '@/interfaces': './src/interfaces',
-            '@/main': './src/main',
-            '@/models': './src/models',
-            '@/services': './src/services',
-            '@/shared': './src/shared',
-            '@/ui': './src/ui',
-            '@/validation': './src/validation',
-            '@/test': './test',
-            '@/jest': './jest',
-          },
-        },
-      ],
-      'react-native-reanimated/plugin',
-    ],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
